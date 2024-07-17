f62a22acb3aa0fd94def44f57838e940b8d05ff8
AspieSoft <aspiesoftweb@gmail.com>
2024-07-17 10:58:13 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TermSplit Plugin For Micro
+
+This plugin adds a keybinding `Alt-p` to open a smaller hsplit terminal pane.
+This reflects how vscode has a terminal shortcut like `Ctrl+Shift+,`.
+
+Install by cloning this into your plug directory:
+
+```shell
+git clone https://github.com/AspieSoft/micro-editor-plugin-termsplit ~/.config/micro/plug/termsplit
+```
---
+++ termsplit.lua
@@ -0,0 +1,20 @@
+local micro = import("micro")
+local config = import("micro/config")
+local shell = import("micro/shell")
+local fmt = import("fmt")
+
+function init()
+  config.MakeCommand("tsplit", termTab, config.NoComplete)
+  config.TryBindKey("Alt-p", "command:tsplit", false)
+end
+
+function termTab(bp)
+  bp:HSplitAction()
+  local tab = bp:Tab()
+  local pane = tab:CurPane()
+  local id = pane:ID()
+  local node = tab:GetNode(id)
+  pane:ResizePane(node.Y + 7)
+
+  pane:HandleCommand("term")
+end
